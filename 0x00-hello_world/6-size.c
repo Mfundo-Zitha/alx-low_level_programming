f14d6f1a91f8d49c2555636961b25c57786ca852
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-08 01:58:01 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	char d;
-	float f;
-	printf("size of a char: 1 byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of an int: 4 byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of a long int: 8 byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of a long long int: 1 byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+int a;
+long int b;
+char d;
+float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
