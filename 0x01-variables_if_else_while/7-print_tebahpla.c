ff3b98907b2cd6dc803223220d7cd7e1cb6f5606
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-09 13:54:27 -0700

    done

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-
 /**
-** main - program that prints all single digit numbers of base 10 starting from
-** You are not allowed to use any variable of type char
-** You can only use the putchar function
-** Return: 0
-**/
-
+* main - program that prints all single digit numbers of base 10 starting from
+* You are not allowed to use any variable of type char
+* You can only use the putchar function
+* Return: 0
+*/
 int main(void)
 {
-	int n = 48;
-	while (n <= 57)
-	{
-		putchar(n);
-		n += 1;
-	}
-	printf("\n");
-	return (0);
+char c;
+for (c = 'Z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+printf("\n");
+return (0);
 }
