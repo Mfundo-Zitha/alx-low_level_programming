b5bd206db03a8d44dab27cc836815a3baef1fe2e
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-09 13:22:02 -0700

    lets see

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++i)
 {
-if (ch != 'e' && ch != 'q')
-{
-    putchar(ch);
-}
+       if (ch != 'e' && ch != 'q')
+         {
+          putchar(ch);
+         }
 }
 putchar('\n');
 return (0);
