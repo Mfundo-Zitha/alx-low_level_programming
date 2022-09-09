93324dabf83d5f91d4eca89aafd43a7e9d1561bb
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-09 14:26:40 -0700

    ok

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * main -  program that prints all the numbers of base 16 in lowercase
- * Description
+ * Descriptin
  * You can only use the putchar function
  * Return: 0
  */
 int main(void)
 {
 	int c;
-	
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
