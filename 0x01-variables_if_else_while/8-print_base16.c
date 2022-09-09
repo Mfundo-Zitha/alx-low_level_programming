306cc29a46026ef4faf38b22d167f30c17f20907
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-09 14:06:52 -0700

    Lets go

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
 /**
- ** main - program that prints the lowercase alphabet in reverse.
- ** You can only use the putchar function
- ** Return: 0
- **/
-
+ * main - program that prints the lowercase alphabet in reverse.
+ *
+ * You can only use the putchar function
+ * Return: 0
+ */
 int main(void)
 {
-	int n = 122;
-	while (n > 96)
-	{
-		putchar(n);
-		n -= 1;
-	}
-	putchar('\n');
-	return (0);
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
 }
