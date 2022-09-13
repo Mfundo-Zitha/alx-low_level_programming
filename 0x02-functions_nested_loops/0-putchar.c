6348d1f2b007b268d602bc5866a074e8972c95a0
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-13 04:10:20 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,16 @@
 * main - prints putchar
 * Return: Always 0
 */
+
 int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-i	_putchar('t');
+	_putchar('t');
 	_putchar('c');
-	_putchar(_'h');
-	_putchar(_'a');
+	_putchar('h');
+	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
