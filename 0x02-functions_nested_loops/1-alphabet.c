b9a2898a7ec0ee5e3007bebabb45518d5838068a
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-13 02:41:56 -0700

    fixed changes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * prints_alphabet -> prints the lowercase alphabets
+ */
+
+void prints_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
