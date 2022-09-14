1be18cd971759da22c3d4e2b7e46832c0843da2f
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-13 20:59:16 -0700

    done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
- * add - Adds two integrers.
+ * add - Adds @num1 and @num2 integrers.
  * @numl: The first integer to be added.
  * @num2: The second integer to be added.
- *
+ * Description @num1: yes
+ * Description @num2
+ * num1
+ * num2
  * Return: The result of the addition.
  */
-int add(int num1, int numb2)
+int add(int num1, int num2)
 {
-	return (num1 + numb2);
+	return (num1 + num2);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void times_table(void);
-int add(int, int);
+int add(int num1, int num2);
 void print_to_98(int n);
