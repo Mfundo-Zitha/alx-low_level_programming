0059ac694a3eac245e0732f46f73f15417aff800
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-13 20:12:54 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two integrers.
+ * @numl: The first integer to be added.
+ * @numl: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num1, int numb2)
+{
+	return (num1 + numb2)
+}
