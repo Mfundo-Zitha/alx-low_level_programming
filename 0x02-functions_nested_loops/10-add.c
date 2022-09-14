2898790780b315a8430ab2f224d1ef4fa83ddc1d
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-13 21:17:21 -0700

    done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
 
 /**
  * add - Adds @num1 and @num2 integrers.
- * @numl: The first integer to be added.
+ * @num1: The first integer to be added.
  * @num2: The second integer to be added.
- * Description: @num1: yes
+ * 
  * Description: @num2
  * @num1
  * @num2
