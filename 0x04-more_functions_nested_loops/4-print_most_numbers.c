bc719d19d43df96ce475873f70d22438b46b03bc
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-15 03:40:42 -0700

    dpen

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers -> print numbers
+ * print_most_numbers -> print numbers
  */
 
 void print_numbers(void)
@@ -9,6 +9,9 @@ void print_numbers(void)
 	int x;
 
 	for (x = 0; x < 10; x++)
-		_putchar(x + '0');
+	{
+		if (x != 2 && x != 4)
+			_putchar(x + '0');
+	}
 	_putchar('\n');
 }
