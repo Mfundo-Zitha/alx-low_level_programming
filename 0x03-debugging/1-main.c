a15350e30dc7d876d1f25ec74606ab543bc75b18
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-14 23:20:09 -0700

    done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,11 @@ int i;
 printf("Infinite lopp incoming :(\n");
 
 i = 0;
-
-/**
-* while (i < 10)
-* {
-* _putchar(i);
-* }
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
 */
 printf("Infinite loop avoided! \\o/\n");
 
