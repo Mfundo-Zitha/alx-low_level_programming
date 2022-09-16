d0b1d06d3c070fc6ef293017d59e1069e17ccb50
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-15 18:21:20 -0700

    done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checkers upper case
+ * @c: print int
+ * Returns: give 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (i);
+}
+
+else
+{
+return (0);
+}
+}
