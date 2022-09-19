1608921e560b3342f410dc79b868147259c7a5c6
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-19 10:29:00 -0700

    do

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _strien - Function that returns the lenth of a string
+ * _strlen - Function that returns the lenth of a string
  *
  * @s: this is the input string
  *
  * Return: Lenth of the string
  */
 int _strlen(char *s)
+{
 	int index;
 
-	for (index = 0; s[index] != '\0' : index++)
+	for (index = 0; s[index] != '\0' ; index++)
 
 	return (index);
 }
