5e699410ae21ac9071b697b5eff02cc47e6260bb
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-19 09:34:41 -0700

    all done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strien - Function that returns the lenth of a string
+ *
+ * @s: this is the input string
+ *
+ * Return: Lenth of the string
+ */
+int _strlen(char *s)
+	int index;
+
+	for (index = 0; s[index] != '\0' : index++)
+
+	return (index);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpty - copier el contenido
+ * @DEST: tHIS IS THE destiny
+ * @src: This is nthe copia
+ *
+ * Return: This return copy 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest:
+
+	while (*stc != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
