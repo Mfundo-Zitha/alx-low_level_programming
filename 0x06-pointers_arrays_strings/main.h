88be22f27c3917c5589751c153b53ec0ed68a19d
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-22 17:35:54 -0700

    doen

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 /**
  * File: main.h
- * Auth: sam tech(samuel birhanu)
+ * Auth: Mfundo Zitha
  *
  * Desc: Header file containing protye
  */
@@ -22,3 +22,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
+#endif
