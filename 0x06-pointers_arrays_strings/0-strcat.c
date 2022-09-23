96113f7e4ba0b3960ca6b0aa966e68c76fbc494b
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-09-22 18:07:52 -0700

    dood

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.>
+#include <string.h>
 
 /**
  * _strcat - Concatenates the string pointed to by @src, including the terminat
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,6 @@
  *
  * Desc: Header file containing prototype of all functions
  */
-
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
