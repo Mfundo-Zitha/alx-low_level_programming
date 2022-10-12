162c6312b9478ed7348aaf1b464b9afdea630636
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-10-12 08:12:42 -0700

    half way done

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sum - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - calculatesa the product of a n b
+ * @a: first interger
+ * @b: second integer
+ *
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - c
+ * @a: a
+ * @b: n
+ *
+ * Return: the dic
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - c
+ * @a: a
+ * @b: b
+ * Return: remainder
+ */
+ int op_mod(int a, int b)
+{
+	return (a % b);
+}
