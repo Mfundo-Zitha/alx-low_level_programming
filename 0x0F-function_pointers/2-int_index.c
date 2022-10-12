10f6d429385247766ce01f6add3eba8c4778568d
Mfundo-Zitha <mwlszitha0606@gmail.com>
2022-10-12 07:02:28 -0700

    done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers"
+/**
+ * int_index - earches for an integer
+ * @array: array to search
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Rerun: index of the first element
+ * the cmp functiondoes not return 0
+ */
+int int_index(int *array,int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
+}
